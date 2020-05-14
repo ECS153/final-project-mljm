a4a5861c632229b005f5765a50cf576def232c6f
cjh010 <cjh0102010@gmail.com>
2020-05-14 00:50:45 -0700

    add some string manipulation before storing

--- sourcecode/db.py
+++ sourcecode/db.py
@@ -1,6 +1,35 @@
 import json
 import os
 
+#functions that may be useful to manipulate string before storing
+def addchar(string, mode):
+    result = ""
+    length = len(string)
+    char_code = 97
+    for i in range(length):
+        #for every fixed number of character add a character
+        #I can't make the character random right now because I can't easily get the same key again
+        if i % mode == 0:
+            result += chr(char_code)
+            char_code += 1
+            if char_code > 122:
+                char_code = 64
+        result += string[i]
+    return result
+
+def removechar(string, mode):
+    result = ""
+    length = len(string)
+    count = 0
+    for i in range(1,length):
+        if count != mode:
+            result += string[i]
+            count += 1
+        else:
+            count = 0
+    return result
+
+#store and retrive functionality using simple json
 class Database():
     def __init__(self, disk):
         #disk is the path of the file we store the data in
@@ -34,7 +63,11 @@ class Database():
 
     def insert(self, nickname, username, password):
         try:
-            self.db[str(nickname)] = (username,password)
+            nickname = addchar(nickname,1)
+            username = addchar(username,2)
+            password = addchar(password,3)
+            self.db[nickname] = (username,password)
+            print("stored a record")
             return True
         except:
             print("error happen when inserting ")
@@ -42,15 +75,22 @@ class Database():
     
     def get(self, nickname):
         try:
-            return self.db[nickname]
+            nickname = addchar(nickname,1)
+            (username, password) = self.db[nickname]
+            username = removechar(username,2)
+            password = removechar(password,3)
+            print("got a record")
+            return (username,password)
         except:
             print("error getting data")
-            pass
+            return (None, None)
 
     def delete(self, nickname):
+        nickname = addchar(nickname,1)
         if not nickname in self.db:
             return False
         del self.db[nickname]
+        print("deleted a record")
         return True
 
 
--- sourcecode/test.py
+++ sourcecode/test.py
@@ -1,18 +1,24 @@
 from db import *
+from crypt import *
 
 db = Database("./ourdb")
 db.open("username", "password")
 
-db.insert("boa", "bsdux333", "dasxc32156dv")
-db.insert("boa2", "czxzcvc", "24sf84yr33r")
-db.insert("amazon", "dvuxdsafvi", "123456")
-db.insert("google", "username", "password")
+key = "11364592315"
+mycry = MyCrypt()
 
-print(db.get("amazon"))
-print(db.get("google"))
-db.delete("boa")
-print(db.get("boa"))
+db.insert(mycry.AES_Encrypt(key, "boa"), mycry.AES_Encrypt(key, "bsdux333"), mycry.AES_Encrypt(key, "dasxc32156dv"))
+db.insert(mycry.AES_Encrypt(key, "boa2"), mycry.AES_Encrypt(key, "asdfdvcxv"), mycry.AES_Encrypt(key, "32dxcd622165"))
+db.insert(mycry.AES_Encrypt(key, "google"), mycry.AES_Encrypt(key, "baxcxcz"), mycry.AES_Encrypt(key, "d15165649595dv"))
+db.insert(mycry.AES_Encrypt(key, "amazon"), mycry.AES_Encrypt(key, "adgfhjtfd"), mycry.AES_Encrypt(key, "das51c54vd46asds"))
 
+(a,b) = db.get(mycry.AES_Encrypt(key, "boa"))
+if a != None and b != None:
+    print(mycry.AES_Decrypt(key,a),mycry.AES_Decrypt(key,b))
+(a,b) = db.get(mycry.AES_Encrypt(key, "google"))
+if a != None and b != None:
+    print(mycry.AES_Decrypt(key,a),mycry.AES_Decrypt(key,b))
+db.delete(mycry.AES_Encrypt(key, "boa"))
+print(db.get(mycry.AES_Encrypt(key, "boa")))
 db.close()
-
 
